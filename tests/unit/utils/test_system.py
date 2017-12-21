e2207daa65e1a01be07ae25d9dd5e017f5b0fb7b
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-12-21 19:40:53 +0300

    Проверка определения покрытия тестами

--- tests/unit/utils/test_system.py
+++ tests/unit/utils/test_system.py
@@ -1,8 +1,8 @@
 from hydra_agent.utils import is_linux, is_windows, is_mac
 
 
-def test_system_detection():
-    assert (is_windows() + is_linux() + is_mac()) == 1
+# def test_system_detection():
+#     assert (is_windows() + is_linux() + is_mac()) == 1
 
 
 def test_codecov():
