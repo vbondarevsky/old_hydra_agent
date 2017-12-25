ddf7c8d03407bf5fa7d1e655c0a05649a3be533e
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-12-26 01:13:17 +0300

    Упрощено наименование методов

--- tests/unit/ring/license_manager/test_list.py
+++ tests/unit/ring/license_manager/test_list.py
@@ -11,7 +11,7 @@ from tests.unit import success_result, fail_result
     ('332588056123456-8100123456\n173211456654321-800654321\n',
      ['332588056123456-8100123456', '173211456654321-800654321'])
 ], ids=['empty', 'one', 'few'])
-def test_list_success(monkeypatch, fake_license_manager, out, expected):
+def test_success(monkeypatch, fake_license_manager, out, expected):
     monkeypatch.setattr(subprocess, 'run', lambda args, **kwargs: success_result(args, out))
     assert fake_license_manager.list() == expected
 
@@ -20,7 +20,7 @@ def test_list_success(monkeypatch, fake_license_manager, out, expected):
     (('Ошибка получения списка лицензий.\nПо причине: Ошибка при работе с хранилищем лицензий.\n'
       ' По причине: Директория не найдена: "/var/lic".\n'), 1, '/var/lic'),
 ], ids=['wrong_path'])
-def test_list_fail(monkeypatch, fake_license_manager, out, return_code, path):
+def test_fail(monkeypatch, fake_license_manager, out, return_code, path):
     monkeypatch.setattr(subprocess, 'run', lambda args, **kwargs: fail_result(args, return_code, out))
     with pytest.raises(subprocess.CalledProcessError):
         fake_license_manager.list(path=path)
