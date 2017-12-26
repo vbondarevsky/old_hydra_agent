ffa038f65fca4be609a95431cd3ceb97d153af1a
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-12-26 16:45:16 +0300

    Убрал закомментированный код

--- tests/unit/utils/test_system.py
+++ tests/unit/utils/test_system.py
@@ -1,11 +1,8 @@
 from hydra_agent.utils import is_linux, is_windows, is_mac
 
 
-# def test_system_detection():
-#     assert (is_windows() + is_linux() + is_mac()) == 1
-
-
 def test_codecov():
+    result = None
     if is_windows():
         result = True
     elif is_mac():
