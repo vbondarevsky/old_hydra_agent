5386dde680019aa8360ebd3f55b40df50dede551
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-12-29 02:52:22 +0300

    Прописал лицензию в файлы с кодом

---
+++ tests/unit/rac/cluster_manager/__init__.py
@@ -0,0 +1,15 @@
+# This file is part of HYDRA - cross-platform remote administration
+# system for 1C:Enterprise (https://github.com/vbondarevsky/hydra_agent).
+# Copyright (C) 2017  Vladimir Bondarevskiy.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
