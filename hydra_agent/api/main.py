605949948c8f0d4820cc6fd63c2ede88d9d375e5
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-12-28 15:29:40 +0300

    Упрощенный вариант heartbeat

--- hydra_agent/api/main.py
+++ hydra_agent/api/main.py
@@ -2,8 +2,12 @@ from aiohttp import web
 
 from hydra_agent.api.routes import setup_routes
 
+DEBUG = True  # TODO: вынести в конфиг
+HOST = '127.0.0.1'  # TODO: вынести в конфиг
+PORT = 9523  # TODO: вынести в конфиг
+
 
 def run_server():
-    app = web.Application()
+    app = web.Application(debug=DEBUG)
     setup_routes(app)
-    web.run_app(app, host='127.0.0.1', port=9523)
+    web.run_app(app, host=HOST, port=PORT)
--- hydra_agent/api/routes.py
+++ hydra_agent/api/routes.py
@@ -1,5 +1,11 @@
+from hydra_agent.api.handler.heartbeat import HeartbeatHandler
 from hydra_agent.api.handler.license import LicenseHandler
 
 
+def build_route(route):
+    return f'/hydra_agent/api/v1{route}'
+
+
 def setup_routes(app):
-    app.router.add_route('*', '/license', LicenseHandler)
+    app.router.add_route('*', build_route('/license'), LicenseHandler)
+    app.router.add_route('*', build_route('/heartbeat'), HeartbeatHandler)
