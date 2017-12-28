06051646266b9dae3784fc55f3386bd3214e72c1
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2017-12-28 15:28:59 +0300

    Удалил лишний вызов

--- hydra_agent/api/handler/license.py
+++ hydra_agent/api/handler/license.py
@@ -5,5 +5,4 @@ from hydra_agent import license_manager
 
 class LicenseHandler(web.View):
     async def get(self):
-        license_manager.list()
         return web.json_response(license_manager.list())
